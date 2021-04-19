83f6587691109d4004c791ecefd872d219616f73
Peter Schafhalter <pschafhalter@berkeley.edu>
2021-04-19 15:30:30 -0700

    Add gitignore and cleanup

--- build/CMakeFiles/FrenetOptimalTrajectory_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/FrenetOptimalTrajectory_autogen"
-  "FrenetOptimalTrajectory_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/FrenetOptimalTrajectory_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
